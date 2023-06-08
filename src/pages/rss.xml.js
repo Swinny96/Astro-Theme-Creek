f7534902219df0a3841f8eab79c86188e92e176a
Christopher Swinton <chris@theswintons.co.uk>
2023-06-08 23:29:12 +0100

    Changed Creek for Enghouse Portfolio

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,8 +5,8 @@ const posts = Object.values(postImportResult);
 
 export const get = () =>
   rss({
-    title: "Astro Theme Creek",
-    description: "A Theme for Astro",
+    title: "Enghouse Portfolio",
+    description: "Christopher Swintons Enghouse Portfolio",
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/**/*.md"),
   });
